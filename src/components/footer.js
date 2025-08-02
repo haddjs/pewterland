67b75a41892483e5258964ae9676d32071b1d235
haddjs <nadirhexonyte@gmail.com>
2025-08-02 16:14:02 +0700

    footer revision

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,18 +2,18 @@ import React from "react";
 
 const Footer = () => {
 	return (
-		<div className="text-white text-sm flex justify-center gap-20 pb-8">
+		<div className="text-white w-full text-sm flex flex-nowrap justify-center gap-20 pb-8">
 			<h1>
-				<strong>WhatsApp: </strong>
-				<i>+62 812 3456 7890</i>
+				<span className="font-bold">WhatsApp: </span>
+				<span className="italic">+62 812 3456 7890</span>
 			</h1>
 			<h1>
-				<strong>Email: </strong>
-				<i>pewterland.official@gmail.com</i>
+				<span className="font-bold">Email: </span>
+				<span className="italic">pewterland.official@gmail.com</span>
 			</h1>
 			<h1>
-				<strong>Instagram: </strong>
-				<i>@pewterland.official</i>
+				<span className="font-bold">Instagram: </span>
+				<span className="italic">@pewterland.official</span>
 			</h1>
 		</div>
 	);
